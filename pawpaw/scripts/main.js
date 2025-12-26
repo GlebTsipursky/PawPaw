124fd88363e9de1b2c66b7e79ce129bae810d7e3
GlebTsipursky <gleb11@gmail.com>
2025-12-26 13:56:54 -0500

    Add files via upload

---
+++ pawpaw/scripts/main.js
@@ -0,0 +1,36 @@
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/Fruit_of_pawpaw,_with_seeds_revealed.jpg") {
+    myImage.setAttribute("src", "images/Pawpaw fruits held in hand.jpg");
+  } else {
+    myImage.setAttribute(
+      "src",
+      "images/Fruit_of_pawpaw,_with_seeds_revealed.jpg",
+    );
+  }
+});
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Pawpaws are cool, ${myName}`;
+  }
+}
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Pawpaws are cool, ${storedName}`;
+}
+myButton.addEventListener("click", () => {
+  setUserName();
+});
